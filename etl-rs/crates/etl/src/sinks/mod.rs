f1dbd0f0697ab3c2ac88db1d5eed0a01f8b875e5
MarsPresLai <marslai23@icloud.com>
2026-05-27 11:23:07 +0800

    chore: remove old etl-rs files

--- etl-rs/crates/etl/src/sinks/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod clickhouse;
-pub mod neo4j;
-pub mod postgres_reader;
-pub mod postgres_writer;
-pub mod redis_consumer;
-pub mod redis_stream;
-
-pub use redis_stream::{RedisStreamWriter, StdoutWriter, TransactionWriter};
